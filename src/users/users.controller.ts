2febab0a1e53f16f0f7caf04a4e46e99b32aa17a
DaniLucano10 <olucano10@gmail.com>
2025-07-12 02:00:17 -0500

    cambiar el estado de usuario

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiBearerAuth, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 @ApiBearerAuth('JWT-auth')
 @Controller('users')
 export class UsersController {
@@ -25,11 +27,13 @@ export class UsersController {
 
   @Get()
   @ApiOperation({ summary: 'Listar los usuarios' })
-  //@ApiQuery({ name: 'status', type: Boolean, required: false })
+  @ApiQuery({ name: 'status', type: Boolean, required: false })
   @ApiQuery({ name: 'email', type: String, required: false })
   @ApiQuery({ name: 'fullname', type: String, required: false })
-  find() {
-    return this.usersService.find();
+  find(
+    @Query() query: { status?: boolean; email?: string; fullname?: string },
+  ) {
+    return this.usersService.find(query);
   }
 
   @Get(':id')
@@ -38,8 +42,15 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.usersService.update(+id);
+  @ApiOperation({ summary: 'Actualizar un usuario' })
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Patch('toggle-status/:id')
+  @ApiOperation({ summary: 'Cambiar el estado de un usuario' })
+  toggleStatus(@Param('id') id: string) {
+    return this.usersService.toggleStatus(+id);
   }
 
   @Delete(':id')
