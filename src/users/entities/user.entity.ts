3ce19abb9d6d326d4715904921f874a4d3ec4e3d
DaniLucano10 <olucano10@gmail.com>
2025-07-11 23:43:50 -0500

    colocando ! a las entidades

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,25 +11,25 @@ import {
 export class User {
   @PrimaryGeneratedColumn()
   @ApiProperty()
-  id: number;
+  id!: number;
 
   @Column({ unique: false })
   @ApiProperty()
-  fullname: string;
+  fullname!: string;
 
   @Column({ type: 'varchar', length: 100, unique: true })
   @ApiProperty()
-  email: string;
+  email!: string;
 
   @Column({ type: 'varchar', length: 100 })
   @ApiProperty()
-  password: string;
+  password!: string;
 
   @CreateDateColumn()
   @ApiProperty()
-  created_at: Date;
+  created_at!: Date;
 
   @UpdateDateColumn()
   @ApiProperty()
-  updated_at: Date;
+  updated_at!: Date;
 }
