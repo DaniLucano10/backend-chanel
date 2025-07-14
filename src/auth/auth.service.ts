e23297b92f76a679fef85845539d2de215881235
DaniLucano10 <olucano10@gmail.com>
2025-07-14 17:56:59 -0500

    texto en español

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      throw new UnauthorizedException('pasword is wrong');
+      throw new UnauthorizedException('contraseña incorrecta');
     }
 
     const payload = {
