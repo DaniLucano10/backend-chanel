574b25f92c529ff792f5d35f178ebf480dd49c8b
DaniLucano10 <olucano10@gmail.com>
2025-07-14 16:55:22 -0500

    modificando el nombre del guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth/guard/auth.guard';
+import { JwtAuthGuard } from './auth/guard/auth.guard';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -31,7 +31,7 @@ import { ConfigModule } from '@nestjs/config';
     AppService,
     {
       provide: APP_GUARD,
-      useClass: AuthGuard,
+      useClass: JwtAuthGuard,
     },
   ],
 })
