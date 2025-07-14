5ca7fba31d20458a46fad7c72f2e2118d010b618
DaniLucano10 <olucano10@gmail.com>
2025-07-14 16:57:54 -0500

    verificar si el token es valido

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,27 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy as JwtPassportStrategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstants } from '../constants/jwt.constant';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BlacklistedToken } from '../entities/blacklisted-token.entity';
+import { ActiveToken } from '../entities/active-token.entity';
 
 interface JwtPayload {
   sub: number;
+  username: string;
   email: string;
 }
 
 @Injectable()
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-export class JwtStrategy extends PassportStrategy(JwtPassportStrategy) {
-  constructor() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(ActiveToken)
+    private readonly activeTokenRepository: Repository<ActiveToken>,
+    @InjectRepository(BlacklistedToken)
+    private readonly blacklistTokenRepository: Repository<BlacklistedToken>,
+  ) {
     super({
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
+      passReqToCallback: true,
     });
   }
 
-  validate(payload: JwtPayload) {
-    return { id: payload.sub, email: payload.email };
+  async validate(req: Request, payload: JwtPayload) {
+    const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+    if (!token) {
+      throw new UnauthorizedException('Token no encontrado');
+    }
+
+    const blacklistedToken = await this.blacklistTokenRepository.findOne({
+      where: { token },
+    });
+
+    if (blacklistedToken) {
+      throw new UnauthorizedException('Token revocado');
+    }
+
+    const activeToken = await this.activeTokenRepository.findOne({
+      where: { email: payload.email, token },
+    });
+
+    if (!activeToken) {
+      throw new UnauthorizedException('Token inválido');
+    }
+
+    return {
+      sub: payload.sub,
+      username: payload.username,
+      email: payload.email,
+    };
   }
 }
