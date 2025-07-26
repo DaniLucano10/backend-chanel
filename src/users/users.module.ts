b438e0c96b2a9fc9e4a0156b635a2497b765f29b
DaniLucano10 <olucano10@gmail.com>
2025-07-26 00:24:04 -0500

    importando modulos

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,20 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Country } from '../country/entities/country.entity';
+import { Role } from '../role/entities/role.entity';
 import { RoleHasPermission } from '../role_has_permission/entities/role_has_permission.entity';
+import { UserHasRole } from '../user_has_role/entities/user_has_role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Country, RoleHasPermission])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Country,
+      RoleHasPermission,
+      Role,
+      UserHasRole,
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
